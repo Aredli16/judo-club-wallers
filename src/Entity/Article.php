80960a4800eb8f40039a58bf449d60f75fcc38bd
Mathis <matblairon@gmail.com>
2022-12-19 12:43:10 +0100

    add slug into Article entity

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -29,6 +29,9 @@ class Article
     #[ORM\Column]
     private ?\DateTimeImmutable $created_at = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -90,6 +93,18 @@ class Article
     public function setCreatedAt(\DateTimeImmutable $created_at): self
     {
         $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
