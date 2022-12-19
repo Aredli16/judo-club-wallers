d492fb8765a32d980881359be0e2a7d86173f4ce
Mathis <matblairon@gmail.com>
2022-12-19 13:54:10 +0100

    feat: show articles and onclick show specific article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
-    #[Route('/article', name: 'app_article')]
-    public function index(): Response
+    #[Route('/article', name: 'show.articles')]
+    public function showArticles(ManagerRegistry $doctrine): Response
     {
-        return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
-        ]);
+        $articles = $doctrine->getRepository(Article::class)->findAll();
+        return $this->render('articles/index.html.twig', ['articles' => $articles]);
     }
+
+    #[Route('/article/{id}', name: 'show.article')]
+    public function showArticle(ManagerRegistry $doctrine, $id): Response
+    {
+        $article = $doctrine->getRepository(Article::class)->find($id);
+        return $this->render('article/index.html.twig', ['article' => $article]);
+    }
+
 }
