a725e78bb65f06231518cb2f1d3e7e74ea1c2783
Lempereur Corentin <73390812+Aredli16@users.noreply.github.com>
2022-12-23 22:29:42 +0100

    refactor: change entity manager to repository to see in code coverage

--- tests/Repository/AlbumRepositoryTest.php
+++ tests/Repository/AlbumRepositoryTest.php
@@ -4,37 +4,36 @@ namespace App\Tests\Repository;
 
 use App\Entity\Album;
 use App\Entity\Image;
-use Doctrine\Persistence\ObjectManager;
+use App\Repository\AlbumRepository;
+use App\Repository\ImageRepository;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class AlbumRepositoryTest extends KernelTestCase
 {
-    private ObjectManager $entityManager;
-    private $repository;
+    private AlbumRepository $albumRepository;
+    private ImageRepository $imageRepository;
 
     public function testAlbumIsSave()
     {
-        $albums = $this->repository->findAll();
+        $albums = $this->albumRepository->findAll();
         $countAlbums = count($albums);
 
         $album = (new Album())
             ->setTitle('Test');
-        $this->entityManager->persist($album);
-        $this->entityManager->flush();
+        $this->albumRepository->save($album, true);
 
-        $albums = $this->repository->findAll();
+        $albums = $this->albumRepository->findAll();
         $this->assertCount($countAlbums + 1, $albums);
     }
 
     public function testAlbumIsDelete()
     {
-        $albums = $this->repository->findAll();
+        $albums = $this->albumRepository->findAll();
         $countAlbums = count($albums);
 
-        $this->entityManager->remove($albums[rand(0, $countAlbums - 1)]);
-        $this->entityManager->flush();
+        $this->albumRepository->remove($albums[rand(0, $countAlbums - 1)], true);
 
-        $albums = $this->repository->findAll();
+        $albums = $this->albumRepository->findAll();
         $this->assertCount($countAlbums - 1, $albums);
     }
 
@@ -42,21 +41,19 @@ class AlbumRepositoryTest extends KernelTestCase
     {
         $album = (new Album())
             ->setTitle('Test album');
-        $this->entityManager->persist($album);
-        $this->entityManager->flush();
+        $this->albumRepository->save($album, true);
 
-        $albums = $this->repository->findAll();
+        $albums = $this->albumRepository->findAll();
         $album = end($albums);
 
         $this->assertEquals('test-album-' . $album->getId(), $album->getSlug());
 
-        $this->entityManager->remove($album);
-        $this->entityManager->flush();
+        $this->albumRepository->remove($album, true);
     }
 
     public function testPhotoIsSaveWithAlbum()
     {
-        $photos = $this->entityManager->getRepository(Image::class)->findAll();
+        $photos = $this->imageRepository->findAll();
         $countPhotos = count($photos);
 
         $album = (new Album())
@@ -64,24 +61,22 @@ class AlbumRepositoryTest extends KernelTestCase
             ->addPhoto((new Image())
                 ->setName('Photo test'));
 
-        $this->entityManager->persist($album);
-        $this->entityManager->flush();
+        $this->albumRepository->save($album, true);
 
-        $photos = $this->entityManager->getRepository(Image::class)->findAll();
+        $photos = $this->imageRepository->findAll();
         $this->assertCount($countPhotos + 1, $photos);
     }
 
     public function testPhotoIsDeleteWithAlbum()
     {
-        $albums = $this->repository->findAll();
+        $albums = $this->albumRepository->findAll();
         $countAlbums = count($albums);
-        $photos = $this->entityManager->getRepository(Image::class)->findAll();
+        $photos = $this->imageRepository->findAll();
         $countPhotos = count($photos);
 
-        $this->entityManager->remove($albums[rand(0, $countAlbums - 1)]);
-        $this->entityManager->flush();
+        $this->albumRepository->remove($albums[rand(0, $countAlbums - 1)], true);
 
-        $photos = $this->entityManager->getRepository(Image::class)->findAll();
+        $photos = $this->imageRepository->findAll();
         $this->assertCount($countPhotos - 1, $photos);
     }
 
@@ -90,7 +85,7 @@ class AlbumRepositoryTest extends KernelTestCase
         parent::setUp();
         $kernel = self::bootKernel();
 
-        $this->entityManager = $kernel->getContainer()->get('doctrine')->getManager();
-        $this->repository = $this->entityManager->getRepository(Album::class);
+        $this->albumRepository = $kernel->getContainer()->get('doctrine')->getManager()->getRepository(Album::class);
+        $this->imageRepository = $kernel->getContainer()->get('doctrine')->getManager()->getRepository(Image::class);
     }
 }
