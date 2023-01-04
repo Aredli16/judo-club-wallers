e0d973d58de7093fede980ba41d7d0e348564780
Lempereur Corentin <73390812+Aredli16@users.noreply.github.com>
2023-01-04 16:24:39 +0100

    feat: show last album home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\AlbumRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(
+        private readonly AlbumRepository $albumRepository
+    )
+    {
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $lastAlbums = $this->albumRepository->findLastAlbum(3);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'lastAlbums' => $lastAlbums
         ]);
     }
 }
