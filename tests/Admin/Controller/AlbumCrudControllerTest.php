cf39f874d2cb106fe1684920f27f8ed8e915616e
Lempereur Corentin <73390812+Aredli16@users.noreply.github.com>
2023-01-04 09:31:52 +0100

    test: make admin auth test

--- tests/Admin/Controller/AlbumCrudControllerTest.php
+++ tests/Admin/Controller/AlbumCrudControllerTest.php
@@ -2,15 +2,64 @@
 
 namespace App\Tests\Admin\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AlbumCrudControllerTest extends WebTestCase
 {
+    private KernelBrowser $client;
+    private UserRepository $userRepository;
+    private User $userTest;
+
     public function testAlbumCrudResponseSuccessful(): void
     {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/admin?crudAction=index&crudControllerFqcn=App\Controller\Admin\AlbumCrudController');
+        $this->client->loginUser($this->userTest);
+        $this->client->request('GET', '/admin?crudAction=index&crudControllerFqcn=App\Controller\Admin\AlbumCrudController');
 
         $this->assertResponseIsSuccessful();
+    }
+
+    public function testAlbumCrudResponseAccessDeniedIfNotAdmin()
+    {
+        $this->userTest->setRoles([]);
+
+        $this->client->loginUser($this->userTest);
+        $this->client->request('GET', '/admin?crudAction=index&crudControllerFqcn=App\Controller\Admin\AlbumCrudController');
+
+        $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function testAlbumCrudResponseRedirectIfNotLogged()
+    {
+        $this->client->request('GET', '/admin?crudAction=index&crudControllerFqcn=App\Controller\Admin\AlbumCrudController');
+
+        $this->assertResponseRedirects();
+        $this->client->followRedirect();
+        $this->assertRouteSame('app_login');
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->userRepository = static::getContainer()->get(UserRepository::class);
+        $this->userTest = (new User())
+            ->setLastname('Test')
+            ->setFirstname('Test')
+            ->setEmail('test@email.com')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword('Test');
+        $this->userRepository->save($this->userTest, true);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->userRepository->remove($this->userRepository->find($this->userTest->getId()), true);
     }
 }
--- tests/Admin/Controller/DashboardControllerTest.php
+++ tests/Admin/Controller/DashboardControllerTest.php
@@ -2,15 +2,64 @@
 
 namespace App\Tests\Admin\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DashboardControllerTest extends WebTestCase
 {
-    public function testDashboardResponseSuccessful(): void
+    private KernelBrowser $client;
+    private UserRepository $userRepository;
+    private User $userTest;
+
+    public function testDashboardCrudResponseSuccessful(): void
     {
-        $client = static::createClient();
-        $client->request('GET', '/admin');
+        $this->client->loginUser($this->userTest);
+        $this->client->request('GET', '/admin');
 
         $this->assertResponseIsSuccessful();
+    }
+
+    public function testDashboardCrudResponseAccessDeniedIfNotAdmin()
+    {
+        $this->userTest->setRoles([]);
+
+        $this->client->loginUser($this->userTest);
+        $this->client->request('GET', '/admin');
+
+        $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function testDashboardCrudResponseRedirectIfNotLogged()
+    {
+        $this->client->request('GET', '/admin');
+
+        $this->assertResponseRedirects();
+        $this->client->followRedirect();
+        $this->assertRouteSame('app_login');
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->userRepository = static::getContainer()->get(UserRepository::class);
+        $this->userTest = (new User())
+            ->setLastname('Test')
+            ->setFirstname('Test')
+            ->setEmail('test@email.com')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword('Test');
+        $this->userRepository->save($this->userTest, true);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        $this->userRepository->remove($this->userRepository->find($this->userTest->getId()), true);
     }
 }
