2f8458e04642963f63c13e33a9d5cb49a291ecba
Lempereur Corentin <73390812+Aredli16@users.noreply.github.com>
2023-01-04 15:39:51 +0100

    feat: feat show an album

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Album;
 use App\Repository\AlbumRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,13 +16,21 @@ class AlbumController extends AbstractController
     {
     }
 
-    #[Route('/album', name: 'app_album')]
+    #[Route('/album', name: 'app_album', methods: ['GET'])]
     public function index(): Response
     {
         $albums = $this->albumRepository->findAll();
 
         return $this->render('album/index.html.twig', [
             'albums' => $albums
+        ]);
+    }
+
+    #[Route('/album/{slug}', name: 'app_show.album', methods: ['GET'])]
+    public function showAlbum(Album $album): Response
+    {
+        return $this->render('album/show.html.twig', [
+            'album' => $album
         ]);
     }
 }
