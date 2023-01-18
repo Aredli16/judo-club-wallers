d0ef3eec7c41b88aca104ccad4494345b000fe6c
Shinotobira <62753305+Shinotobira@users.noreply.github.com>
2023-01-18 17:09:08 +0100

    Fix : redirection to admin dashbord

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -47,7 +47,7 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('admin dashboard'));
+        return new RedirectResponse($this->urlGenerator->generate('admin'));
     }
 
     protected function getLoginUrl(Request $request): string
