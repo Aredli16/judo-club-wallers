d67cd9587ea43c16f523268f88fadf8c26448ecd
Lempereur Corentin <73390812+Aredli16@users.noreply.github.com>
2023-01-20 01:37:15 +0100

    feat: feat implement teachers into about page

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class AboutController extends AbstractController
 {
     #[Route('/about', name: 'app_about')]
-    public function index(): Response
+    public function index(
+        UserRepository $userRepository
+    ): Response
     {
+        $teachers = $userRepository->findByRole('ROLE_TEACHER');
+
         return $this->render('about/index.html.twig', [
-            'controller_name' => 'AboutController',
+            'teachers' => $teachers
         ]);
     }
 }
